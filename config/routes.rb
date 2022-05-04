270707322bf3d360d9aad9075b98fee681663225
robinlashae1 <robinlashae1@gmail.com>
2022-05-04 15:22:11 -0400

    added db

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :schools, only: [:index, :destroy, :create, :show]
+  scope '/api/version1' do
+  resources :schools, only: [:index, :destroy, :create, :show]
 resources :users
 resources :service_categories do
   resources :user_services
@@ -13,4 +14,5 @@ get "/me", to: "users#show"
 post "/login", to: "sessions#create"
 delete "/logout", to: "sessions#destroy"
 get "*path", to: "fallback#index", constraints: -> (req) { !req.xhr? && req.format.html? }
+end
 end
