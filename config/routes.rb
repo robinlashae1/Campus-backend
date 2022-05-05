cf2b3619b821d2d027c7ea5c49e71e9f0aefaf1f
robinlashae1 <robinlashae1@gmail.com>
2022-05-05 01:40:03 -0400

    default page

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ post "/signup", to: "users#create"
 get "/me", to: "users#show"
 post "/login", to: "sessions#create"
 delete "/logout", to: "sessions#destroy"
-get "*path", to: "fallback#index", constraints: -> (req) { !req.xhr? && req.format.html? }
+
 end
+get "*path", to: "fallback#index", constraints: -> (req) { !req.xhr? && req.format.html? }
+root "application#index"
 end
